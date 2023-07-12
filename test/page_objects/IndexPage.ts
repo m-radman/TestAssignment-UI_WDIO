525ea34410e27e7a79fbec45e5cabdada71e6b95
m-radman <milrad@live.com>
2023-07-12 09:34:53 +0200

    Add Index Page PO

---
+++ test/page_objects/IndexPage.ts
@@ -0,0 +1,30 @@
+
+class IndexPage {
+  static url: string = "https://www.saucedemo.com/"
+
+  private usernameInputField: string
+  private passwordInputField: string
+  private loginButton: string
+
+  constructor() {
+    this.usernameInputField = "#user-name"
+    this.passwordInputField = "#password"
+    this.loginButton = "#login-button"
+  }
+
+  async open() {
+    await browser.url(IndexPage.url)
+  }
+
+  async enterUsername(username: string) {
+    await $(this.usernameInputField).setValue(username)
+  }
+
+  async enterPassword(password: string) {
+    await $(this.passwordInputField).setValue(password)
+  }
+
+  async loginUser() {
+    await $(this.loginButton).click()
+  }
+}
