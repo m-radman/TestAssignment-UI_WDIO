f6580dc2f2386018bfa4e1eef4bcf21a4aea141b
m-radman <milrad@live.com>
2023-07-13 13:07:18 +0200

    Add run in headless mode

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -58,7 +58,10 @@ export const config: Options.Testrunner = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['headless', 'disable-gpu']
+        }
     }],
 
     //
